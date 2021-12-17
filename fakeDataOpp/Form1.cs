5d88667f7259e5f4038a28847fab7fd4914a64a8
Murat Karasah <mkarasah@gmail.com>
2021-12-17 12:18:07 +0300

    Add project files.

---
+++ fakeDataOpp/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fakeDataOpp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Ogrenci> olist = new List<Ogrenci>(); 
+            for (int i = 1; i < 100; i++)
+            {
+                Ogrenci ogr = new Ogrenci();
+                ogr.Id = i;
+                ogr.ad = FakeData.NameData.GetFirstName();
+                ogr.soyad = FakeData.NameData.GetSurname();
+                ogr.telefon = FakeData.PhoneNumberData.GetPhoneNumber();
+                olist.Add(ogr);
+            }
+
+            dataGridView1.DataSource = olist;
+        }
+    }
+}
---
+++ fakeDataOpp/Ogrenci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fakeDataOpp
+{
+    public class Ogrenci
+    {
+        public int Id
+            {
+            get;
+            set;
+            }
+
+
+        public string ad
+        {
+            get;
+            set;
+        }
+
+
+        public string soyad
+        {
+            get;
+            set;
+        }
+
+
+        public string telefon
+        {
+            get;
+            set;
+        }
+
+
+     
+    }
+}
